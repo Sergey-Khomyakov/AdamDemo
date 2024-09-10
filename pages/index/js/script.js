68f722bf4c06e02d6eda34bf7dee8c4bd8553e47
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-10 10:48:23 +0300

    update contacts in cloud

--- pages/index/js/script.js
+++ pages/index/js/script.js
@@ -2,26 +2,28 @@ $(document).ready( async function() {
     try {
         window.Telegram.WebApp.expand();
         window.Telegram.WebApp.ready();
-        const keys = await getKeysFromStorage();
         debugger
-        getItemFromStorage('userContacts')
-            .then((data) => {
-                if (!data) {
-                    window.Telegram.WebApp.requestContact((isAccess, res) =>{
-                        if(isAccess){
-                            setItemToStorage('userContacts', res)
+        getItemFromStorage('userPhone')
+        .then((data) => {
+            if (!data) {
+                window.Telegram.WebApp.requestContact((isAccess, res) =>{
+                    if(isAccess){
+                        const phone = res.responseUnsafe?.contact?.phone_number;
+                        if(phone !== null && phone !== undefined){
+
+                            setItemToStorage('userPhone', phone)
                             .then((data) => {
                                 if (data) {
                                     console.log('success')
                                 }
                             });
-                        }else{
-                            // Запросить разрешение на получение контакта
                         }
-                    });
-                }
-            });
-
+                    }else{
+                        // Запросить разрешение на получение контакта
+                    }
+                });
+            }
+        });
     } catch (e) {
         console.log(e)
     }

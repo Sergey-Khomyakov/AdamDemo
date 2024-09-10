0c8c27d91d4942c0914e2d687569d44c14c16fa0
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-10 10:08:31 +0300

    update contacts in cloud

--- pages/index/js/script.js
+++ pages/index/js/script.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 
     window.Telegram.WebApp.ready();
 
-    debugger
-    const userContacts = window.Telegram.WebApp.CloudStorage.getItem("userContacts", (data) =>{
+
+    window.Telegram.WebApp.CloudStorage.getItem('userContacts')
+    .then((data) => {
         if(data === null){
             window.Telegram.WebApp.requestContact((isAccess, res) =>{
                 if(isAccess){
@@ -13,6 +14,22 @@ $(document).ready(function() {
                 }
             });
         }
-        window.Telegram.WebApp.BackButton.hide();
+        console.log('Retrieved data:', data);
+    })
+    .catch((error) => {
+        console.error('Error retrieving data:', error)
     });
+    debugger
+    // const userContacts = window.Telegram.WebApp.CloudStorage.getItem("userContacts", (data) =>{
+    //     if(data === null){
+    //         window.Telegram.WebApp.requestContact((isAccess, res) =>{
+    //             if(isAccess){
+    //                 window.Telegram.WebApp.CloudStorage.setItem("userContacts", res);
+    //             }else{
+    //                 // Запросить разрешение на получение контакта
+    //             }
+    //         });
+    //     }
+    //     window.Telegram.WebApp.BackButton.hide();
+    // });
 });

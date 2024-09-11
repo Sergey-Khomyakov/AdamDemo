3b4c911a432ace71b10c566827464e87c40ee608
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-11 15:17:18 +0300

    remove test page

--- js/test.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( async function() {
-    try {
-        window.Telegram.WebApp.shareToStory("https://adamweb.ru/local/templates/adam/img/other/carousel__design/Screen4.png")
-    } catch (e) {
-        console.log(e)
-    }
-});

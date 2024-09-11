a58a1ab4fe250f2ac0063ff3d32c4f0d30d45540
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-11 11:07:47 +0300

    update

--- pages/index/js/script.js
+++ pages/index/js/script.js
@@ -4,27 +4,45 @@ $(document).ready( async function() {
         window.Telegram.WebApp.ready();
         window.Telegram.WebApp.BackButton.hide();
 
-        getItemFromStorage('userPhone')
-        .then((data) => {
-            if (!data) {
-                window.Telegram.WebApp.requestContact((isAccess, res) =>{
-                    if(isAccess){
-                        const phone = res.responseUnsafe?.contact?.phone_number;
-                        if(phone !== null && phone !== undefined){
 
-                            setItemToStorage('userPhone', phone)
-                            .then((data) => {
-                                if (data) {
-                                    console.log('success')
+        if (window.Telegram?.WebApp?.initDataUnsafe) {
+            const initData = window.Telegram?.WebApp?.initData;
+
+            fetch('http://192.168.0.101:3030/api/verify', {
+                method: 'POST',
+                body: JSON.stringify({
+                    initData: initData
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((res) => {
+                if (res.ok) {
+                    getItemFromStorage('userPhone')
+                    .then((data) => {
+                        if (!data) {
+                            window.Telegram.WebApp.requestContact((isAccess, res) =>{
+                                if(isAccess){
+                                    const phone = res.responseUnsafe?.contact?.phone_number;
+                                    if(phone !== null && phone !== undefined){
+            
+                                        setItemToStorage('userPhone', phone)
+                                        .then((data) => {
+                                            if (data) {
+                                                console.log('success')
+                                            }
+                                        });
+                                    }
+                                }else{
+                                    // Запросить разрешение на получение контакта
                                 }
                             });
                         }
-                    }else{
-                        // Запросить разрешение на получение контакта
-                    }
-                });
-            }
-        });
+                    });
+                }
+            })
+        }
+
     } catch (e) {
         console.log(e)
     }

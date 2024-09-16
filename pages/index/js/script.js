34bfbebe6c140fc35af4918eba49750257c0019b
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-16 22:38:41 +0300

    update

--- pages/index/js/script.js
+++ pages/index/js/script.js
@@ -5,22 +5,6 @@ $(document).ready( async function() {
         window.Telegram.WebApp.BackButton.hide();
         window.Telegram.WebApp.disableVerticalSwipes();
 
-        if (window.Telegram?.WebApp?.initDataUnsafe) {
-            const initData = window.Telegram?.WebApp?.initData;
-
-            fetch('https://adamwebdemo.duckdns.org/api/verify', {
-                method: 'POST',
-                body: JSON.stringify({ initData: initData }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            }).then((res) => {
-                if (res.ok) {
-
-                }
-            })
-        }
-
     } catch (e) {
         console.log(e)
     }

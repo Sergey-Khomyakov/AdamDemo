9c3227c7b69cc191120c008eb8f6a24a717e46f0
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-16 08:39:33 +0300

    update link photo

--- pages/portal/js/master.js
+++ pages/portal/js/master.js
@@ -125,9 +125,18 @@ $(document).ready(function() {
             'Content-Type': 'application/json',
         }
     }).then((res) => {
+        // if (res.ok) {
+        //     const result = JSON.parse(res.body);
+        //     $('.widget__lk__info .widget__lk__img img, .page__departament .card.employee .card__img').attr('src', "data:image/png;base64, " + result.photo);
+        // }
         if (res.ok) {
-            const result = JSON.parse(res.body);
-            $('.widget__lk__info .widget__lk__img img, .page__departament .card.employee .card__img').attr('src', "data:image/png;base64, " + result.photo);
+            return res.json(); // Correctly parse the JSON response
+        } else {
+            throw new Error('Network response was not ok');
         }
-    })
+    }).then((result) => {
+        $('.widget__lk__info .widget__lk__img img, .page__departament .card.employee .card__img').attr('src', "data:image/png;base64, " + result.photo);
+    }).catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
 })

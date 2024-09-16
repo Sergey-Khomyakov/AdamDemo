c60cfcaf79f89044b0e461b69cec9332bbafe4d7
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-09-16 22:23:24 +0300

    update

--- pages/bank/js/script.js
+++ pages/bank/js/script.js
@@ -31,7 +31,23 @@ $(document).ready(function() {
         const firstName = window.Telegram.WebApp.initDataUnsafe?.user?.first_name || "";
         $('.widget__lk__info .widget__lk__fio, .page__departament .employee__box .text[data-field="userFIO"]').text(lastName + " " + firstName);
         $('.card.card__bank .text.text-fio').text(lastName + " " + firstName);
-        $()
+
+        fetch('https://adamwebdemo.duckdns.org/api/getUserPhotoBase64?userId=' + window.Telegram.WebApp.initDataUnsafe?.user?.id + '', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          }
+      }).then((res) => {
+          if (res.ok) {
+              return res.json(); // Correctly parse the JSON response
+          } else {
+              throw new Error('Network response was not ok');
+          }
+      }).then((result) => {
+          $('.widget__lk__info .widget__lk__img img').attr('src', "data:image/png;base64, " + result.photo);
+      }).catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error);
+      });
     }
 
     window.Telegram.WebApp.BackButton.show();
--- pages/orders/js/script.js
+++ pages/orders/js/script.js
@@ -50,6 +50,23 @@ $(document).ready(function() {
         const lastName = window.Telegram.WebApp.initDataUnsafe?.user?.last_name || "";
         const firstName = window.Telegram.WebApp.initDataUnsafe?.user?.first_name || "";
         $('.widget__lk__info .widget__lk__fio, .page__departament .employee__box .text[data-field="userFIO"]').text(lastName + " " + firstName);
+
+        fetch('https://adamwebdemo.duckdns.org/api/getUserPhotoBase64?userId=' + window.Telegram.WebApp.initDataUnsafe?.user?.id + '', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then((res) => {
+            if (res.ok) {
+                return res.json(); // Correctly parse the JSON response
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        }).then((result) => {
+            $('.widget__lk__info .widget__lk__img img').attr('src', "data:image/png;base64, " + result.photo);
+        }).catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
     }
 
     window.Telegram.WebApp.BackButton.show();

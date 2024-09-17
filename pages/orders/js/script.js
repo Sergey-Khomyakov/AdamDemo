1ac69101260fe1c6e617049bd8f891220409d619
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2024-09-17 16:57:04 +0300

    update orders

--- pages/orders/js/script.js
+++ pages/orders/js/script.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
                             message: "В чат отправлена геолокация"
                         });
                         $card.find('.btn.btn-primary').addClass("hidden");
-                        $card.find('.text-status').removeClass("text-grey").addClass("text-violet").html("<span>• </span> Принят").text("Принят");
+                        $card.find('.text-status').removeClass("text-grey").addClass("text-violet").html("<span>• </span> Принят");
                     }
                 }).catch((error) => {
                     console.error('There has been a problem with your fetch operation:', error);

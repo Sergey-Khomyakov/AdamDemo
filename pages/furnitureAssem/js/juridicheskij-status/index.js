5a547eb4b47cd309d57cf0ddb95408a9f72c7c84
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2024-11-29 11:49:33 +0300

    update furnitureAssem

---
+++ pages/furnitureAssem/js/juridicheskij-status/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('div[docId"]').on('click', '[docDonwload]', function(){
+        const $btn = $(this);
+        const $parent = $btn.closest('div[docId]');
+        const docId = $parent.attr('docId');
+
+        if(docId === "1"){
+            window.Telegram.WebApp.downloadFile({
+                url: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+                file_name: 'test.pdf'
+            })
+        }
+        else if(docId === "2"){
+            window.Telegram.WebApp.downloadFile({
+                url: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+                file_name: 'test2.pdf'
+            })
+        }
+    });
+});
